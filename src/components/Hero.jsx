1655eb4f3444ebb281f80a3d72706a539c0c0922
rahul darla <darla.rahul@gmail.com>
2023-05-01 19:15:27 +0530

    react icon added to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,12 @@
 import { motion } from "framer-motion";
 import { styles } from "../styles";
+import Computers from "./canvas/Computers";
 
 const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[200px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
@@ -22,6 +23,8 @@ const Hero = () => {
           </p>
         </div>
       </div>
+      <Computers />
+
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,73 +1,47 @@
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { Float, Line, Sphere } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React, { Suspense, useEffect, useState } from "react";
-import CanvasLoader from "../Loader";
+import { useMemo } from "react";
+import * as THREE from "three";
 
-const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/react_logo.glb");
-
+export default function Computers() {
   return (
-    <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
-      <pointLight intensity={1} />
-      <primitive
-        object={computer.scene}
-        scale={isMobile ? 2 : 0.75}
-        position={isMobile ? [0, -0.5, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />
-    </mesh>
-  );
-};
-
-const ComputersCanvas = () => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    setIsMobile(mediaQuery.matches);
-
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
-    };
-
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
-  }, []);
-
-  return (
-    <Canvas
-      frameloop="demand"
-      shadows
-      dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          autoRotate
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Computers isMobile={isMobile} />
-      </Suspense>
-
-      <Preload all />
+    <Canvas camera={{ position: [0, 0, 15] }}>
+      <Float speed={3} rotationIntensity={3} floatIntensity={40}>
+        <Atom />
+      </Float>
     </Canvas>
   );
-};
+}
 
-export default ComputersCanvas;
+function Atom(props) {
+  const points = useMemo(
+    () =>
+      new THREE.EllipseCurve(0, 0, 3, 1.15, 0, 2 * Math.PI, false, 0).getPoints(
+        100
+      ),
+    []
+  );
+  return (
+    <group {...props}>
+      <Line worldUnits points={points} color="turquoise" lineWidth={0.3} />
+      <Line
+        worldUnits
+        points={points}
+        color="mediumPurple"
+        lineWidth={0.3}
+        rotation={[0, 0, 1]}
+      />
+      <Line
+        worldUnits
+        points={points}
+        color="violet"
+        lineWidth={0.3}
+        rotation={[0, 0, -1]}
+      />
+
+      <Sphere args={[0.55, 64, 64]}>
+        <meshBasicMaterial color={[6, 5, 2]} toneMapped={false} />
+      </Sphere>
+    </group>
+  );
+}
